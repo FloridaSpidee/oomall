5585ce819185bbba8ce81ec253abaa3a8166d6ed
leipipi <1195389634@qq.com>
2021-12-21 20:28:54 +0800

    12/21

---
+++ liquidation/src/main/java/cn/edu/xmu/other/liquidation/model/vo/SimpleUserRetVo.java
@@ -0,0 +1,17 @@
+package cn.edu.xmu.other.liquidation.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Auther hongyu lei
+ * @Date 2021/12/21
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleUserRetVo {
+    Long id;
+    String name;
+}
