4065748da6c2eda0e476b9b729316736ce778698
Chen Yixuan <18358944184@163.com>
2021-12-21 21:20:59 +0800

    更新时间的注解

--- liquidation/src/main/java/cn/edu/xmu/other/liquidation/model/vo/RevenueRetVo.java
+++ liquidation/src/main/java/cn/edu/xmu/other/liquidation/model/vo/RevenueRetVo.java
@@ -1,11 +1,13 @@
 package cn.edu.xmu.other.liquidation.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 /**
  * @Author Chen Yixuan
@@ -26,7 +28,9 @@ public class RevenueRetVo {
     private Long shopRevenue;
     private Long expressFee;
     private SimpleObject creator;
-    private LocalDateTime gmtCreate;
-    private LocalDateTime gmtModified;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSXXX",timezone = "GMT+8")
+    private ZonedDateTime gmtCreate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSXXX",timezone = "GMT+8")
+    private ZonedDateTime gmtModified;
     private SimpleObject modifier;
 }
